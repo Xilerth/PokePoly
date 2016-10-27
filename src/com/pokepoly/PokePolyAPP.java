1422db9b135e843d84ba74aab78f3f5942a4e8f6
GabriMagic <GabriMagic@pokeply.com>
2016-10-27 17:06:58 +0100

    Test
    Change-Id: Ib1b14a1cc4d8f713b08b9209466ff1268edc2ada
    Signed-off-by: Xilerth <Xilerth@gmail.com>

---
+++ src/com/pokepoly/PokePolyAPP.java
@@ -0,0 +1,20 @@
+package pokepoly.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PokePolyAPP extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		primaryStage.setTitle("PokéPoly");
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+}
