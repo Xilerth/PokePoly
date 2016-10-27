494598d325a03dcb0fb4b8d511a7a0a908219729
Xilerth <Xilerth@gmail.com>
2016-10-27 19:46:08 +0100

    Pokepoly
    Cambios app

---
+++ src/com/pokepoly/PokePolyApp.java
@@ -0,0 +1,17 @@
+package com.pokepoly;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PokePolyApp extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
