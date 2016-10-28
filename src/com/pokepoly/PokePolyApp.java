40de930d3fb81cccaf1ca42577cd0195a6e9e047
Xilerth <Xilerth@gmail.com>
2016-10-28 09:37:41 +0100

    spaces

--- src/com/pokepoly/PokePolyApp.java
+++ src/com/pokepoly/PokePolyApp.java
@@ -1,5 +1,7 @@
 package com.pokepoly;
 
+import java.util.Iterator;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -9,8 +11,7 @@ public class PokePolyApp extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		
 		System.out.println("gabri es manca");
-		
-	}
+			}
 	public static void main(String[] args) {
 		launch(args);
 	}
