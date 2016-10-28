b9127d6ea614a6d9221c7c8db8b448c799268de9
GabriMagic <champy9a@gmail.com>
2016-10-28 09:52:40 +0100

    Gabri
    pene

--- src/com/pokepoly/PokePolyApp.java
+++ src/com/pokepoly/PokePolyApp.java
@@ -1,17 +1,21 @@
 package com.pokepoly;
 
-import java.util.Iterator;
-
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class PokePolyApp extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
-		System.out.println("gabri es manca");
-			}
+
+		primaryStage.setScene(new Scene(new HBox(5, new Button("PENE")), 620, 620));
+		primaryStage.setTitle("PokéPoly");
+		primaryStage.show();
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
