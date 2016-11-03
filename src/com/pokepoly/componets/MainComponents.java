dfac4d550e451e471276f24a11e36b769efc0216
Xilerth <Xilerth@gmail.com>
2016-11-03 12:41:33 +0000

    Cambio para que funcione...

--- src/com/pokepoly/componets/MainComponents.java
+++ src/com/pokepoly/componets/MainComponents.java
@@ -1,13 +1,23 @@
 package com.pokepoly.componets;
 
+import com.pokepoly.controllers.DadosController;
+
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class MainComponents extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
+		DadosController dados = new DadosController();
+		primaryStage.setScene(new Scene(dados.getDadoButton(),200,200));
+		primaryStage.setTitle("PokéPoly");
+		primaryStage.initStyle(StageStyle.UNIFIED);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
--- src/com/pokepoly/controllers/DadosController.java
+++ src/com/pokepoly/controllers/DadosController.java
@@ -9,16 +9,14 @@ public class DadosController {
 	DadoButton dadoButton;
 
 	public DadosController() {
-		dados = new DadosModel();
 		dadoButton = new DadoButton();
 
 		dadoButton.setOnAction((e)-> onDadosButtonAction());
-		System.out.println("asdas");
 	}
 
 	private void onDadosButtonAction() {
-		dados.lanzarDados(1);
-		System.out.println("pene");
+		dados = new DadosModel(6);
+		dados.lanzarDados(2);
 	}
 
 	public DadosModel getDados() {
@@ -28,4 +26,11 @@ public class DadosController {
 	public void setCaras(int caras) {
 		dados = new DadosModel(caras);
 	}
+
+	public DadoButton getDadoButton() {
+		return dadoButton;
+	}
+	
+	
+	
 }
