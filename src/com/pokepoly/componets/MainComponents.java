b5af2dd5719f27a5eb185a8ef295b419047f3b75
GabriMagic <champy9a@gmail.com>
2016-11-03 11:56:44 +0000

    MainComponents

---
+++ src/com/pokepoly/componets/MainComponents.java
@@ -0,0 +1,17 @@
+package com.pokepoly.componets;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainComponents extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
