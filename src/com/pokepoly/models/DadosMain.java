fdc51744871a6d30eca886d264174952bb84c533
GabriMagic <champy9a@gmail.com>
2016-11-04 10:44:46 +0000

    asd

--- src/com/pokepoly/models/DadosMain.java
+++ src/com/pokepoly/models/DadosMain.java
@@ -1,22 +1,21 @@
 package com.pokepoly.models;
 
-import com.pokepoly.controllers.DadosController;
-
 public class DadosMain {
 
 	public static void main(String[] args) {
-/*
-		DadosController dadosController = new DadosController();
-		
-//		dadosController.getDados() = new DadosModel(6);
-
-		dadosController.getDados().lanzarDados(3);
-		System.out.println(dadosController.getDados().getResultadoTotal());
-		System.out.println(dadosController.getDados().getDados());
-
-		dadosController.getDados().lanzarDados(3);
-		System.out.println(dadosController.getDados().getResultadoTotal());
-		System.out.println(dadosController.getDados().getDados());*/
+		/*
+		 * DadosController dadosController = new DadosController();
+		 * 
+		 * // dadosController.getDados() = new DadosModel(6);
+		 * 
+		 * dadosController.getDados().lanzarDados(3);
+		 * System.out.println(dadosController.getDados().getResultadoTotal());
+		 * System.out.println(dadosController.getDados().getDados());
+		 * 
+		 * dadosController.getDados().lanzarDados(3);
+		 * System.out.println(dadosController.getDados().getResultadoTotal());
+		 * System.out.println(dadosController.getDados().getDados());
+		 */
 	}
 
 }
