27b630ff832509c0a3cacf1ce16246c03f66d34b
GabriMagic <champy9a@gmail.com>
2016-11-04 10:28:07 +0000

    img

--- src/com/pokepoly/componets/MainComponents.java
+++ src/com/pokepoly/componets/MainComponents.java
@@ -7,41 +7,39 @@ import com.pokepoly.controllers.DadosController;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class MainComponents extends Application {
-	 Semaphore mutex = new Semaphore(1, true);
-	 Turno turno;
+	Semaphore mutex = new Semaphore(1, true);
+	Turno turno;
 
 	public void start(Stage primaryStage) throws Exception {
 		Turno turno = new Turno(4);
-		
+
 		ArrayList<DadosController> jugadores = new ArrayList<>();
 		for (int i = 0; i < 4; i++) {
-			jugadores.add(new DadosController(mutex,turno,i));
+			jugadores.add(new DadosController(mutex, turno, i));
 		}
-		
 
 		primaryStage.setScene(new Scene(new HBox(), 200, 200));
 		primaryStage.setTitle("PokéPoly");
 		primaryStage.initStyle(StageStyle.UNIFIED);
 		primaryStage.show();
-		
+
 		ArrayList<Stage> jugadoresStages = new ArrayList<>();
 		for (int i = 0; i < 4; i++) {
 			jugadoresStages.add(new Stage());
-			jugadoresStages.get(i).setScene(new Scene(jugadores.get(i).getDadoButton(),200,200));
+			jugadoresStages.get(i).setScene(new Scene(jugadores.get(i).getDadoButton(), 200, 200));
 			jugadoresStages.get(i).initOwner(primaryStage);
-			jugadoresStages.get(i).setTitle("Jugador numero "+(i+1));
-			jugadoresStages.get(i).setX(300*(i+2));
+			jugadoresStages.get(i).setTitle("Jugador numero " + (i + 1));
+			jugadoresStages.get(i).setX(300 * (i + 2));
 			jugadoresStages.get(i).setY(500);
 			jugadoresStages.get(i).initStyle(StageStyle.UNIFIED);
 			jugadoresStages.get(i).show();
-			
+
 		}
-		
+
 	}
 
 	public static void main(String[] args) {
