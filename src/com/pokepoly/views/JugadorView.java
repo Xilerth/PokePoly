1af14d3bb41a79f7ba014474f393ba5fb952d0a1
GabriMagic <champy9a@gmail.com>
2016-11-07 14:12:51 +0000

    dsa
    sad

--- src/com/pokepoly/views/JugadorView.java
+++ src/com/pokepoly/views/JugadorView.java
@@ -3,21 +3,25 @@ package com.pokepoly.views;
 import com.pokepoly.componets.DadoButton;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-public class JugadorView extends GridPane {
+public class JugadorView extends HBox {
 
 	DadoButton dadoButton;
 	Button nextButton, pkmn1, pkmn2, pkmn3, pkmn4, pkmn5, pkmn6;
 	ImageView imagenJugador;
 	Label nombreLabel, dineroLabel, gimnasiosLabel, pokedexLabel;
-	// Label
+	Label dineroTextoLabel, nombreTextoLabel, pokedexTextoLabel, gimnasiosTextoLabel;
+	GridPane gridPane;
 
 	public JugadorView() {
+		super(5);
 
 		dadoButton = new DadoButton();
 
@@ -30,7 +34,8 @@ public class JugadorView extends GridPane {
 
 		imagenJugador = new ImageView(getClass().getResource("../resources/img/asd.jpg").toExternalForm());
 		imagenJugador.setFitWidth(75);
-		imagenJugador.setFitHeight(80);
+		imagenJugador.setFitHeight(85);
+
 		nextButton = new Button();
 		nextButton.setMaxWidth(50);
 		nextButton.setMaxHeight(50);
@@ -39,30 +44,35 @@ public class JugadorView extends GridPane {
 		nombreLabel = new Label("Nombre");
 		dineroLabel = new Label("Dinero");
 		gimnasiosLabel = new Label("Gimnasios");
-		pokedexLabel = new Label("Pok�dex");
+		pokedexLabel = new Label("Pokédex");
 
-		VBox stats = new VBox(5, nombreLabel, dineroLabel, gimnasiosLabel);
+		nombreTextoLabel = new Label("Unown");
+		dineroTextoLabel = new Label("0 ₽");
+		gimnasiosTextoLabel = new Label("0");
+		pokedexTextoLabel = new Label("0 / 151");
 
+		VBox labels = new VBox(5, nombreLabel, dineroLabel, gimnasiosLabel, pokedexLabel);
+		labels.setAlignment(Pos.CENTER);
+
+		VBox dataLabels = new VBox(5, nombreTextoLabel, dineroTextoLabel, gimnasiosTextoLabel, pokedexTextoLabel);
+		dataLabels.setAlignment(Pos.CENTER);
+
+		// EQUIPO POKÉMON
+		gridPane = new GridPane();
 		GridPane.setRowSpan(imagenJugador, 3);
-		GridPane.setRowSpan(stats, 3);
+		gridPane.add(pkmn1, 0, 0);
+		gridPane.add(pkmn2, 1, 0);
+		gridPane.add(pkmn3, 0, 1);
+		gridPane.add(pkmn4, 1, 1);
+		gridPane.add(pkmn5, 0, 2);
+		gridPane.add(pkmn6, 1, 2);
+		gridPane.setVgap(5);
+		gridPane.setHgap(5);
 
-		add(pkmn1, 0, 0);
-		add(pkmn2, 1, 0);
-		add(pkmn3, 0, 1);
-		add(pkmn4, 1, 1);
-		add(pkmn5, 0, 2);
-		add(pkmn6, 1, 2);
+		setPadding(new Insets(5));
+		getChildren().addAll(gridPane, imagenJugador, labels, dataLabels, dadoButton);
 
-		add(imagenJugador, 2, 0);
-		add(stats, 3, 0);
-		// pokedexLabel), 0, 3);
-		// add(nextButton, 2, 3);
-		// add(dadoButton, 0, 4);
-
-		setVgap(5);
-		setHgap(5);
-		setPadding(new Insets(10));
-//		setGridLinesVisible(true);
+		setAlignment(Pos.TOP_CENTER);
 	}
 
 }
