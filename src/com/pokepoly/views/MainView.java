7bd93cfc23d6ef3eb7e638661402bfebb10bfb7f
GabriMagic <champy9a@gmail.com>
2016-11-08 11:27:33 +0000

    asd

--- src/com/pokepoly/views/MainView.java
+++ src/com/pokepoly/views/MainView.java
@@ -6,9 +6,30 @@ import javafx.stage.Stage;
 
 public class MainView extends Application {
 
+	Stage secStage, thrdStage, fourStage;
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		
 		JugadorView v = new JugadorView();
+		JugadorView v2 = new JugadorView();
+		JugadorView v3 = new JugadorView();
+		JugadorView v4 = new JugadorView();
+		
+		secStage = new Stage();
+		secStage.setScene(new Scene(v2));
+		secStage.setTitle("Jugador 2");
+		secStage.show();
+		
+		thrdStage = new Stage();
+		thrdStage.setScene(new Scene(v3));
+		thrdStage.setTitle("Jugador 3");
+		thrdStage.show();
+		
+		fourStage = new Stage();
+		fourStage.setScene(new Scene(v4));
+		fourStage.setTitle("Jugador 4");
+		fourStage.show();
 		
 		primaryStage.setTitle("Jugador 1");
 		primaryStage.setScene(new Scene(v));
